170dc31e99188a61cbba5a2a0f82981e6a8008bf
chenming <mover93@126.com>
2020-10-27 20:44:50 +0800

    认证中心demo

---
+++ oauth2-service/src/main/java/com/chenming/cloud/oauth2/OAuth2ServiceApplication.java
@@ -0,0 +1,18 @@
+package com.chenming.cloud.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description: Oauth2ServiceApplication <br>
+ * author: chenming <br>
+ * date: 2020/10/27 16:43 <br>
+ * version: 1.0 <br>
+ */
+@SpringBootApplication
+public class OAuth2ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2ServiceApplication.class, args);
+    }
+}
