caad5396f415732f2f25dee6bfeb0c125083a111
chenming <mover93@126.com>
2020-10-28 23:07:39 +0800

    fix

--- config-service/src/main/java/com/chenming/cloud/config/ConfigServiceApplication.java
+++ config-service/src/main/java/com/chenming/cloud/config/ConfigServiceApplication.java
@@ -17,34 +17,8 @@ public class ConfigServiceApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(ConfigServiceApplication.class, args);
-//        String userName = applicationContext.getEnvironment().getProperty("user.name");
-//        String userAge = applicationContext.getEnvironment().getProperty("user.age");
-//        System.err.println("user name :"+userName+"; age: "+userAge);
-
-        // 配置动态更新
-//        while(true) {
-//            //当动态配置刷新时，会更新到 Enviroment中，因此这里每隔一秒中从Enviroment中获取配置
-//            String userName = applicationContext.getEnvironment().getProperty("user.name");
-//            String userAge = applicationContext.getEnvironment().getProperty("user.age");
-//            System.err.println("user name :" + userName + "; age: " + userAge);
-//            try {
-//                TimeUnit.SECONDS.sleep(1);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-
-        while(true) {
-            String userName = applicationContext.getEnvironment().getProperty("user.name");
-            String userAge = applicationContext.getEnvironment().getProperty("user.age");
-            //获取当前部署的环境
-            String currentEnv = applicationContext.getEnvironment().getProperty("current.env");
-            System.err.println("in "+currentEnv+" enviroment; "+"user name :" + userName + "; age: " + userAge);
-            try {
-                TimeUnit.SECONDS.sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        String userName = applicationContext.getEnvironment().getProperty("user.name");
+        String userAge = applicationContext.getEnvironment().getProperty("user.age");
+        System.err.println("user name :"+userName+"; age: "+userAge);
     }
 }
